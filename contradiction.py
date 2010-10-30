aeb5fd6b8ada44b4c7a7821930d8d23ce35b0488
Ted Tibbetts <intuited@gmail.com>
2010-10-30 05:48:55 -0300

    Added elements needed to create README.txt and symlink to it from README.rst.
    Also added license terms and improved docstring.

--- contradiction.py
+++ contradiction.py
@@ -8,20 +8,35 @@ def contradiction(dfrom, dto):
     returns (result, value) where
     `result` is '==' or '!=';
     `value` is a dictionary of dictionaries with the keys:
-        '+': keys present in dto but not dfrom
-        '-': keys present in dfrom but not dto
-        '!=': keys present in both with differing values
-        '==': keys present in both with equivalent values.
+
+    -   '+': keys present in dto but not dfrom
+    -   '-': keys present in dfrom but not dto
+    -   '==': keys present in both with equivalent values.
+    -   '!=': keys present in both with differing values
+        -   The values of this dictionary will contain
+            recursively similar structures.
 
     If either or both of `dfrom` and `dto` is a non-dictionary,
     returns ('!=', (dfrom, dto)).
 
-    >>> contradiction({1: 'one', 2: 'two'}, {1: 'one', 2: 'two'})
-    ('==', {1: 'one', 2: 'two'})
-    >>> contradiction({1: {1: 'one', 2: 'two'}}, {1: {1: 'one'}})
-    ('!=', {'!=': {1: {'==': {1: 'one'}, '-': {2: 'two'}}}})
-    >>> contradiction({0: 0, 1: {1: 'uno', 2: 'two'}}, {0: 0, 1: {1: 'one'}})
-    ('!=', {'==': {0: 0}, '!=': {1: {'!=': {1: ('uno', 'one')}, '-': {2: 'two'}}}})
+    Examples::
+
+        >>> from pprint import pprint
+        >>> from functools import partial
+        >>> pp = partial(pprint, width=40)
+        >>> contradiction({1: 'one', 2: 'two'}, {1: 'one', 2: 'two'})
+        ('==', {1: 'one', 2: 'two'})
+        >>> pp(contradiction({1: {1: 'one', 2: 'two'}}, {1: {1: 'one'}}))
+        ('!=',
+         {'!=': {1: {'-': {2: 'two'},
+                     '==': {1: 'one'}}}})
+        >>> pp(contradiction({0: 0, 1: {1: 'uno', 2: 'two'}},
+        ...                  {0: 0, 1: {1: 'one'}}))
+        ('!=',
+         {'!=': {1: {'!=': {1: ('uno',
+                                'one')},
+                     '-': {2: 'two'}}},
+          '==': {0: 0}})
     """
     if isinstance(dfrom, dict) and isinstance(dto, dict):
         results = collections.defaultdict(dict)
